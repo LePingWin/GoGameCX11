7000c7727f1744700d9e8707efbe11ee02cbc146
LePingWin <loic.bine@gmail.com>
2017-01-04 11:44:59 +0100

    Ajout liste de pierres

--- Engine/rules.c
+++ Engine/rules.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include "rules.h"
 
+int getX(int c){
+	return c >> 16;
+}
+
+int getY(int c){
+	return c & 0xFFFF;
+}
+
+int getCoord(int x,int y){
+	return x << 16 | y;
+}
+
 int main()
 {
 	return 0;
